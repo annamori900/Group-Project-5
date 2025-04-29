17caa2989420f9075c8bc2d2d82500afbd699306
anna <150392971+annamori900@users.noreply.github.com>
2025-04-29 16:24:23 -0500

    add sort methods to Shelter model class

--- src/main/java/mvc/controller/ShelterController.java
+++ src/main/java/mvc/controller/ShelterController.java
@@ -1,14 +1,15 @@
 package mvc.controller;
 
 import mvc.model.Shelter;
+import mvc.model.pets.Pet;
 import mvc.view.ShelterView;
 
 public class ShelterController {
 	
-	private Shelter<?> model;
+	private Shelter<Pet> model;
 	private ShelterView view;
 	
-	public ShelterController(Shelter<?> model, ShelterView view) {
+	public ShelterController(Shelter<Pet> model, ShelterView view) {
 		this.model = model;
 		this.view = view;
 		
@@ -17,7 +18,5 @@ public class ShelterController {
 	public void initiate() {
 		view.setVisible(true);
 	}
-	
-	
 
 }
--- src/main/java/mvc/model/Shelter.java
+++ src/main/java/mvc/model/Shelter.java
@@ -1,7 +1,10 @@
 package mvc.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
+import mvc.model.comparators.AgeComparator;
+import mvc.model.comparators.SpeciesComparator;
 import mvc.model.pets.Pet;
 
 public class Shelter<T extends Pet> {
@@ -18,6 +21,18 @@ public class Shelter<T extends Pet> {
 
 	public void setPets(ArrayList<T> pets) {
 		this.pets = pets;
+	}
+	
+	public void sortByName() {
+		Collections.sort(pets);
+	}
+	
+	public void sortByAge() {
+		pets.sort(new AgeComparator());
+	}
+	
+	public void sortBySpecies() {
+		pets.sort(new SpeciesComparator());
 	}
 
 }
