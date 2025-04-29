5fe8d6fdd1b67d2676383a1aa15c507579878130
anna <150392971+annamori900@users.noreply.github.com>
2025-04-29 13:41:01 -0500

    create abstract Pet class

---
+++ src/main/java/mvc/model/pets/Pet.java
@@ -0,0 +1,66 @@
+package mvc.model.pets;
+
+public abstract class Pet implements Comparable<Pet> {
+	
+	protected int id;
+	protected String name;
+	protected String species;
+	protected int age;
+	protected boolean adopted;
+	
+	public Pet(int id, String name, String species, int age, boolean adopted) {
+		setId(id);
+		setName(name);
+		setSpecies(species);
+		setAge(age);
+		setAdopted(adopted);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isAdopted() {
+		return adopted;
+	}
+
+	public void setAdopted(boolean adopted) {
+		this.adopted = adopted;
+	}
+
+	@Override
+	public int compareTo(Pet other) {
+		return this.name.compareToIgnoreCase(other.getName());
+	}
+	
+	public abstract String getType();
+
+}
