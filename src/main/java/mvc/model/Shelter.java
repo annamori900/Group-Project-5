26020992ee77d329ae728406709e98862c084890
anna <150392971+annamori900@users.noreply.github.com>
2025-04-30 16:12:15 -0500

    update Shelter to extend Observable class

--- src/main/java/mvc/model/Shelter.java
+++ src/main/java/mvc/model/Shelter.java
@@ -2,12 +2,14 @@ package mvc.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Observable;
 
 import mvc.model.comparators.AgeComparator;
 import mvc.model.comparators.SpeciesComparator;
 import mvc.model.pets.Pet;
 
-public class Shelter<T extends Pet> {
+@SuppressWarnings("deprecation")
+public class Shelter<T extends Pet> extends Observable {
 	
 	private ArrayList<T> pets;
 	
@@ -21,18 +23,26 @@ public class Shelter<T extends Pet> {
 
 	public void setPets(ArrayList<T> pets) {
 		this.pets = pets;
+		setChanged();
+		notifyObservers();
 	}
 
 	public void sortByName() {
 		Collections.sort(pets);
+		setChanged();
+		notifyObservers();
 	}
 	
 	public void sortByAge() {
 		pets.sort(new AgeComparator());
+		setChanged();
+		notifyObservers();
 	}
 	
 	public void sortBySpecies() {
 		pets.sort(new SpeciesComparator());
+		setChanged();
+		notifyObservers();
 	}
 
 }
